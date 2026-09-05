91ece3f7da8d2ab68a1b6157e44bcb0140bf8ec1
marcossbc <marcossbc880@gmail.com>
2026-09-05 10:18:58 +0300

    aded flatlist

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  FlatList,
+} from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const data = Array.from({ length: 900 }, (_, i) => ({
+    id: i.toString(),
+    text: `Item ${i + 1}`,
+  }));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlatList
+        horizontal={true}
+        showsHorizontalScrollIndicator={true}
+        keyExtractor={(item) => item.id}
+        data={data}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.touchable}
+            onPress={() => alert(item.text)}
+          >
+            <View style={styles.item}>
+              <Text style={styles.itemText}>{item.text}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   );
 }
+
+// const list =({item}) =>{
+//   return (
+//     <Text>
+//       {item.text}
+//     </Text>
+//   )
+// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+
+  item: {
+    backgroundColor: "lightblue",
+    margin: 10,
+    padding: 20,
+    borderRadius: 10,
+  },
+
+  itemText: {
+    fontSize: 20,
+    color: "black",
+  },
+
+  touchable: {
+    marginTop: 10,
   },
 });
